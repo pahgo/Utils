d706b810d322a72785793e21902ee1e73e79f22d
pahgo <franciscoferezsalinas@gmail.com>
2018-01-11 20:21:11 +0100

    DynamicSorter able to sorts strings as numbers Javadoc

--- src/es/monguerapps/utils/sorter/DynamicSorter.java
+++ src/es/monguerapps/utils/sorter/DynamicSorter.java
@@ -1,6 +1,7 @@
 package es.monguerapps.utils.sorter;
 
 import java.lang.reflect.Field;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.Date;
@@ -8,23 +9,105 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * 
+ * @author Pahgo The {@code DynamicSorter<T>} class provides dynamic, easy and
+ *         stable ordering for lists.
+ * @param <T>
+ */
 public class DynamicSorter<T> {
 
 	private String[] fields;
 	private Boolean[] order;
 	private Class<T> clazz;
+	private Boolean[] stringAsNumber;
 
+	/**
+	 * Constructor for general purpose, orders by each field ascending or
+	 * descending depending of true or false in the order array and takes next
+	 * field when two values are equivalent.
+	 * 
+	 * Booth arrays must have same length.
+	 * 
+	 * @param clazz
+	 *            Again the parametrized class.
+	 * @param fields
+	 *            Name of the properties to order in the order to.
+	 * @param order
+	 *            Values for ascending/descending as true/false.
+	 */
 	public DynamicSorter(final Class<T> clazz, String[] fields, Boolean[] order) {
 		this.clazz = clazz;
 		this.fields = fields;
 		this.order = order;
+		this.stringAsNumber = new Boolean[fields.length];
+		Arrays.fill(stringAsNumber, false);
 	}
 
+	/**
+	 * Adds the posibility to order a string as is it where a {@code Double}.
+	 * 
+	 * @param clazz
+	 *            Again the parametrized class.
+	 * @param fields
+	 *            Name of the properties to order in the order to.
+	 * @param order
+	 *            Values for ascending/descending as true/false.
+	 * @param stringAsNumber
+	 *            Take the (if it is a) {@code String} field as a Double if the
+	 *            positions in both arrays are the same.
+	 * @see DynamicSorter#DynamicSorter(Class, String[], Boolean[])
+	 */
+	public DynamicSorter(final Class<T> clazz, String[] fields, Boolean[] order, Boolean[] stringAsNumber) {
+		this.clazz = clazz;
+		this.fields = fields;
+		this.order = order;
+		this.stringAsNumber = stringAsNumber;
+	}
+
+	/**
+	 * Orders the list if the fields, order and stringAsNumber provided are
+	 * still valid.
+	 * 
+	 * @param list
+	 *            The list to order.
+	 */
 	public void reorderList(List<T> list) {
-		reorderList(list, fields, order);
+		reorderList(list, fields, order, stringAsNumber);
 	}
 
+	/**
+	 * Orders the list with new fields and order. Uses the stringAsNumber
+	 * provided.
+	 * 
+	 * @param list
+	 *            The list to order.
+	 * @param fields
+	 *            Name of the properties to order in the order to.
+	 * @param order
+	 *            Values for ascending/descending as true/false.
+	 * @see #reorderList(List)
+	 */
 	public void reorderList(List<T> list, final String[] fields, final Boolean[] order) {
+		reorderList(list, fields, order, stringAsNumber);
+	}
+
+	/**
+	 * Orders the list with new fields, order and stringAsNumber.
+	 * 
+	 * @param list
+	 *            The list to order.
+	 * @param fields
+	 *            Name of the properties to order in the order to.
+	 * @param order
+	 *            Values for ascending/descending as true/false.
+	 * @param strinAsNumber
+	 *            Take the (if it is a) {@code String} field as a Double if the
+	 *            positions in both arrays are the same.
+	 * @see #reorderList(List)
+	 */
+	public void reorderList(List<T> list, final String[] fields, final Boolean[] order,
+			final Boolean[] stringAsNumber) {
 		checkFields(fields);
 		checkLengths(fields, order);
 		Comparator<T> c = new Comparator<T>() {
@@ -32,9 +115,9 @@ public class DynamicSorter<T> {
 				int result = 0;
 				for (int i = 0; i < fields.length; i++) {
 					try {
-						Field f = clazz.getDeclaredField(fields[i]);
-						f.setAccessible(true);
-						result = (order[i] ? 1 : -1) * DynamicSorter.this.compare(f.getType(), f, arg0, arg1);
+						Field f = getFielAccesible(fields[i]);
+						result = (order[i] ? 1 : -1)
+								* DynamicSorter.this.compare(f.getType(), f, arg0, arg1, stringAsNumber[i]);
 						if (result != 0)
 							break;
 					} catch (Exception e) {
@@ -46,8 +129,8 @@ public class DynamicSorter<T> {
 		};
 		Collections.sort(list, c);
 	}
-	private int compare(Class<?> t, Field f, T arg0, T arg1)
-			throws IllegalAccessException {
+
+	private int compare(Class<?> t, Field f, T arg0, T arg1, Boolean stringAsNumber) throws IllegalAccessException {
 		if (t.isAssignableFrom(Boolean.class)) {
 			return ((Boolean) f.get(arg0)).compareTo((Boolean) f.get(arg1));
 		} else if (t.isAssignableFrom(Byte.class)) {
@@ -65,11 +148,13 @@ public class DynamicSorter<T> {
 		} else if (t.isAssignableFrom(Float.class)) {
 			return ((Float) f.get(arg0)).compareTo((Float) f.get(arg1));
 		} else if (t.isAssignableFrom(String.class)) {
+			if (stringAsNumber)
+				return (Double.valueOf((String) f.get(arg0)).compareTo(Double.valueOf((String) f.get(arg1))));
 			return ((String) f.get(arg0)).compareTo((String) f.get(arg1));
 		} else if (t.isAssignableFrom(Date.class)) {
 			return ((Date) f.get(arg0)).compareTo((Date) f.get(arg1));
 		} else if (t.isPrimitive()) {
-			return compare(primitiveWrapperMap.get(f.getType()), f, arg0, arg1);
+			return compare(primitiveWrapperMap.get(f.getType()), f, arg0, arg1, stringAsNumber);
 		} else
 			throw new UnsupportedOperationException(
 					String.format("Camparison not yet implemented for '%s'", f.getType()));
@@ -88,7 +173,7 @@ public class DynamicSorter<T> {
 	}
 
 	private void checkField(Field[] classFields, String field) {
-		boolean exists = false;
+		Boolean exists = false;
 		for (Field f1 : classFields) {
 			if (f1.getName().equals(field)) {
 				exists = true;
@@ -98,6 +183,12 @@ public class DynamicSorter<T> {
 		if (!exists)
 			throw new IllegalArgumentException(
 					String.format("Field %s does not exists in class %s.", field, clazz.getName()));
+	}
+
+	private Field getFielAccesible(String name) throws NoSuchFieldException {
+		Field f = clazz.getDeclaredField(name);
+		f.setAccessible(true);
+		return f;
 	}
 
 	private static final Map<Class<?>, Class<?>> primitiveWrapperMap = new HashMap<Class<?>, Class<?>>();
--- src/es/monguerapps/utils/sorter/example/App.java
+++ src/es/monguerapps/utils/sorter/example/App.java
@@ -1,6 +1,7 @@
 package es.monguerapps.utils.sorter.example;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 
@@ -12,8 +13,9 @@ public class App {
 	public static void main(String[] args) {
 		generatePeople();
 		String[] fields = new String[] { "name", "surname", "height", "birthday" };
-		Boolean[] order = new Boolean[] { false, false, false, true };
-		DynamicSorter<Person> sorter = new DynamicSorter<Person>(Person.class, fields, order);
+		Boolean[] order = new Boolean[] { true, true, false, true };
+		Boolean[] asNum = new Boolean[] { true, false, false, false };
+		DynamicSorter<Person> sorter = new DynamicSorter<Person>(Person.class, fields, order, asNum);
 		sorter.reorderList(people);
 		for (Person p : people)
 			System.out.println(p);
@@ -21,13 +23,20 @@ public class App {
 
 	private static void generatePeople() {
 		Person p;
-		p = new Person("AAA", "AAA", 70, new Date(System.currentTimeMillis() - (23 * 365 * 24 * 60 * 60 * 1000)));
+		p = new Person("1", "BBB", 70, new Date(System.currentTimeMillis() - (23 * 365 * 24 * 60 * 60 * 1000)));
 		people.add(p);
-		p = new Person("B", "BBB", 70, new Date(System.currentTimeMillis() - (23 * 365 * 24 * 60 * 60 * 1000)));
+		p = new Person("1.0", "BBB", 70, new Date(System.currentTimeMillis() - (24 * 365 * 24 * 60 * 60 * 1000)));
 		people.add(p);
-		p = new Person("AAA", "BBB", 71, new Date(System.currentTimeMillis() - (22 * 365 * 24 * 60 * 60 * 1000)));
+		p = new Person("1", "AAA", 70, new Date(System.currentTimeMillis() - (26 * 365 * 24 * 60 * 60 * 1000)));
 		people.add(p);
-		p = new Person("AAA", "AAA", 70, new Date(System.currentTimeMillis() - (21 * 365 * 24 * 60 * 60 * 1000)));
+		p = new Person("10", "BBB", 70, new Date(System.currentTimeMillis() - (23 * 365 * 24 * 60 * 60 * 1000)));
 		people.add(p);
+		p = new Person("9", "BBB", 71, new Date(System.currentTimeMillis() - (22 * 365 * 24 * 60 * 60 * 1000)));
+		people.add(p);
+		p = new Person("11", "AAA", 70, new Date(System.currentTimeMillis() - (21 * 365 * 24 * 60 * 60 * 1000)));
+		people.add(p);
+		p = new Person("1.1", "AAA", 70, new Date(System.currentTimeMillis() - (21 * 365 * 24 * 60 * 60 * 1000)));
+		people.add(p);
+		Collections.shuffle(people);//No real need
 	}
 }
